a74d271e5b6100af7ad2db3017f0a8e90fb80ce0
matisantillan11 <matias1.santillan@gmail.com>
2021-08-24 12:39:06 -0300

    working on integrate mp

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,57 +1,50 @@
 import { useRef, useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 //firebase
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import responses from '../firebase/responses/responses.js';
 
 //Mercado Pago
 //import methods from '../mercadopago/payment-methods/methods';
 
 //styles
-import '../assets/Information.scss'
+import '../assets/Information.scss';
 import AppContext from '../context/AppContext';
-
-
-
+import { mp } from '../api/mercado-pago/mercadopago.js';
 
 const Information = () => {
-    const form = useRef(null);
-    const {state:{ cart }} = useContext(AppContext);
+  const form = useRef(null);
+  const {
+    state: { cart },
+  } = useContext(AppContext);
 
-    
-    useEffect(()=>{
-        firebase.auth().onAuthStateChanged(user =>{
-            if(user){
-               /* form.current.name.value = user.displayName;
-               form.current.email.value = user.email;
-               form.current.phone.value = user.phoneNumber; */
-            } 
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data = { cart: [] };
+    cart.map((item) => data.cart.push(item));
+    console.log(data.cart);
+    /* await mp
+      .post('/checkout', { name: 'matias' })
+      .then((response) => console.log(response.data.json()))
+      .catch((error) => console.log(error)); */
+  };
 
-    }, [])
-
-    
-    return (
-      <div className="Information">
-        <div className="Information-buttons">
-            <div className="Information-back">
-              <Link to="/products/checkout">Regresar</Link>
-            </div>
-          </div>
-        <div className="Information-content">
-          <div className="Information-head">
-            <h2>Informacion de contacto:</h2>
-          </div>
-          <div className="Information-form">
-            <form action="http://localhost:3000/api/mercado-pago/checkout"  method="POST" >
-              {/* <input type="text" placeholder="Nombre Completo" name="name" />
-              <input type="text" placeholder="Correo Electronico" name="email" />
-              <input type="text" placeholder="Direccion" name="address" />
-              <input type="text" placeholder="Ciudad" name="city" />
-              <input type="text" placeholder="Código Postal" name="cp" />
-              <input type="text" placeholder="Teléfono" name="phone" />  */}
+  return (
+    <div className="Information">
+      <div className="Information-buttons">
+        <div className="Information-back">
+          <Link to="/products/checkout">Regresar</Link>
+        </div>
+      </div>
+      <div className="Information-content">
+        <div className="Information-head">
+          <h2>Informacion de contacto:</h2>
+        </div>
+        <div className="Information-form">
+          <form>
+            {/* 
               {cart.map(items => {
                 return (
                   <div>
@@ -61,33 +54,31 @@ const Information = () => {
                     <input type="hidden" name="quantity" value={items.quantity} />
                   </div>
                 )
-              })}
-              
-                
-              
-              <div className="Information-next">
-                <input type="submit" value="Pagar" />
+              })} */}
+
+            <div className="Information-next">
+              <input type="button" value="Pagar" onClick={handleSubmit} />
             </div>
-            </form>
-          </div>
-          
+          </form>
         </div>
-        <div className="Information-sidebar">
-          <h3>Pedido:</h3>
-          {cart.map(item =>{
-          return(
-            <div  className="Information-item" key={item.id}>
+      </div>
+      <div className="Information-sidebar">
+        <h3>Pedido:</h3>
+        {cart.map((item) => {
+          return (
+            <div className="Information-item" key={item.id}>
               <div className="Information-element">
-                
-                <h4>{item.product.name} <span>x{item.quantity}</span></h4>
+                <h4>
+                  {item.product.name} <span>x{item.quantity}</span>
+                </h4>
                 <span>${item.product.price * item.quantity}</span>
               </div>
             </div>
-          )})}
-            
-        </div>
+          );
+        })}
       </div>
-    );
-  };
-  
-  export default Information;
+    </div>
+  );
+};
+
+export default Information;
